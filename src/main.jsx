305d0db0724f1ace9006e3fd58d24280c74ec4e3
shamelna <eng.a.redwan@gmail.com>
2026-03-14 11:08:09 +1100

    Fix asset paths for GitHub Pages deployment

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { seedTestUsers } from './utils/seedTestUsers.js'
+// import { seedTestUsers } from './utils/seedTestUsers.js'
 
 // Initialize test users in development
 // seedTestUsers()
